182429bf7fcb82514a61966a76c00d036ab6da73
TylerWilcock <tyler.l.wilcock@gmail.com>
2015-05-05 15:33:50 -0500

    Updated config to include database login

--- utility_scripts/out.php
+++ utility_scripts/out.php
@@ -5,6 +5,7 @@ ini_set("display_errors", 1);
 
 require_once("/var/www/Sentiment_Analysis/TweetDataGenerator.php");
 require_once("/var/www/Sentiment_Analysis/TweetSanitizer.php");
+require_once("/var/www/Sentiment_Analysis/Database.php");
 require_once("/var/www/Sentiment_Analysis/Dictionary.php");
 require_once("/var/www/Sentiment_Analysis/config.php");
 
@@ -12,11 +13,13 @@ $dictionary = array();
 $tweet_sanitizer = new TweetSanitizer();
 $tweet_data_generator = new TweetDataGenerator(get_oauth_settings());
 $dictionary_inst = new Dictionary();
+$sentiment_db = new Database(get_db_host(), get_db_username(), get_db_password(), "SENTIMENT_ANALYSIS");
 
-$tweets = $tweet_sanitizer->file_read_tokenize_sanitize("/home/pi/twitter_data.txt", "~~~~");
-$dictionary = $dictionary_inst->read_LSD_dictionary("/var/www/Sentiment_Analysis/dictionary/LSD2011.txt", " ", "\n", "*");
+// $tweets = $tweet_sanitizer->file_read_tokenize_sanitize("/home/pi/twitter_data.txt", "~~~~");
+// $dictionary = $dictionary_inst->read_LSD_dictionary("/var/www/Sentiment_Analysis/dictionary/LSD2011.txt", " ", "\n", "*");
 
-$out = $tweet_data_generator->timeline_tweets_to_array("Sethrogen", 200);
+$tweets = $tweet_data_generator->timeline_tweets_to_array("Sethrogen", 200);
+
 
 echo "<pre>";
-print_r($out);
+print_r($tweets);
